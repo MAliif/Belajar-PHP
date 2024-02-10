9af13e9cab923b4090f2e53a083dea96f9e0994c
MAliif <kingfalroy72@gmail.com>
2024-02-10 10:13:11 +0700

    Tipe data

--- Belajar.php
+++ Belajar.php
@@ -29,8 +29,27 @@
     //? Tandak titik (.), digunakan untuk memisahkan string dan variable
     $nama = "Alif";
     $umur = 17;
+
     echo "<h1>Variable</h1>";
     echo "Nama Saya " . $nama . " dan saya berumur " . $umur;
+    echo "<br><br>";
+
+    //* Part 5, Tipe Data
+    //? String, cara penulisannnya menggunakan petik ganda.
+    $v_string = "Tipe data ku string";
+    //? Integer, berbentuk bilangan asli atau bilangan bulat.
+    $v_int = 12;
+    //? Float, bilangan desimal.
+    $v_float = 12.78;
+    //? Boolean, hanya memiliki dua buah value. Yaitu true dan false.
+    $v_boolean = true;
+    $v_boolean2 = false;
+
+    echo "<h1>Tipe Data</h1>";
+    echo "String : ".$v_string."<br>";
+    echo "Integer : ".$v_int."<br>";
+    echo "Float : ".$v_float."<br>";
+    echo "Boolean : ".$v_boolean." dan ".$v_boolean2."<br>";
     echo "<br><br>";
 ?>
 
